0fc02b50979ae81fcdd11d0a3287e1fa43a9fc71
marcelo <jmdicostanzo11@gmail.com>
2018-04-13 18:04:26 -0300

    Cambios

--- UNQfy/unqfy.js
+++ UNQfy/unqfy.js
@@ -12,20 +12,20 @@ class Artist{
 }
 
 class Album{
-	constructor(name, year, artist){
+	constructor(name, year){
 		this.name = name;
     this.year = year;
-    this.artist = artist;
+    this.artist = {};
 		this.tracks = [];
 	}
 }
 
 class Track{
-	constructor(name, duration, genres, album){
+	constructor(name, duration, genres){
 		this.name = name;
 		this.duration = duration;
     this.genres = genres;
-    this.album = album;
+    this.album = {};
 	}
 
 	matchGenres(genres){
@@ -33,9 +33,17 @@ class Track{
 	}
 }
 
-class Playlist{
-  constructor(name){
-    this.name = name;
+// hay q ver si Genero lo ponemos como un objeto
+class Genre{
+  constructor(_name){
+  this.name = _name;
+  }
+};
+
+class PlayList{
+  constructor(_name, _duration){//????
+    this.name = _name;
+    this.duration = _duration;
     this.tracks = [];
   }
 
@@ -49,24 +57,23 @@ class Playlist{
   }
 }
 
-
 //---------------------------------------
 
 class UNQfy {
-
   constructor(){
-    this.artists   = [];
-    this.albums    = [];//TODO: Eliminar lista
-    this.tracks    = [];//TODO: Eliminar lista
-    this.playlists = [];
+    this.artists = [];
+    this.playLists = [];
   }
 
   //TODO: Actualizar (Buscar en la lista de tracks de los albunes de los artistas)
   getTracksMatchingGenres(genres) {
-		return this.tracks.filter(track => track.matchGenres(genres));
+    // corregir ???????????????????????????????????
+		//return this.tracks.filter(track => track.matchGenres(genres));
   }
 
   getTracksMatchingArtist(artist) {
+    // corregir ????????????????????????????????????
+    /*
     const a = artist.albums;    
     let t = [];
 
@@ -74,49 +81,78 @@ class UNQfy {
       if(album.tracks.length !== 0)
         t = t.concat(album.tracks);
     });
-
     return t;
+    */
   }
 
   addArtist(params) {
-		this.artists.push(new Artist(params.name, params.country));
+		let a = new Artist(params.name, params.country);
+    
+    this.artists.push(a);
   }
 
   addAlbum(artistName, params) {
-    let artist = this.getArtistByName(artistName);
-    let album = new Album(params.name, params.year, artist);
-				
-		artist.albums.push(album);
-		this.albums.push(album);//TODO: Eliminar esta linea
+    let album = new Album(params.name, params.year);
+    let artista = this.getArtistByName(artistName);
+    
+    album.artist = artista;
+    artista.albums.push(album);
   }
 
   addTrack(albumName, params) {
-    let album = this.getAlbumByName(albumName);
-    let track = new Track(params.name, params.duration, params.genres, album);
-		
-		album.tracks.push(track);
-		this.tracks.push(track);//TODO: Eliminar esta linea
+    let track = new Track(params.name, params.duration, params.genres);
+		let album = this.getAlbumByName(albumName);
+
+    track.album = album;
+    album.tracks.push(track);
   }
 
   getArtistByName(name) {
-		return this.artists.find(artist => artist.name === name);
+		let a = this.artists.find((artista) => (artista.name === name));
+    
+    if(typeof (a) !== "undefined"){
+      return a;
+    }else{
+      console.log("no hay artista con ese nombre")
+    }
   }
 
   //TODO: Actualizar (Buscar el la lista de albunes de los artistas)
   getAlbumByName(name) {
-		return this.albums.find(album => album.name === name);
+		return this.getAlbums().find((album) => (album.name === name));
+  }
+
+  getAlbums(){
+    let allAlbums = [];
+
+    for (let i in this.artists){
+      let albums = this.artists[i].albums;
+      for(let j in albums){
+        allAlbums.push(albums[j]);  
+      }
+    }
+
+    return allAlbums;
   }
 
   //TODO: Actualizar (Buscar en la lista de tracks de los albunes de los artistas)
   getTrackByName(name) {
-		return this.tracks.find(track => track.name === name);
+		
   }
 
   getPlaylistByName(name) {
-    return this.playlists.find(playlist => playlist.name === name);
+    
   }
 
   addPlaylist(name, genresToInclude, maxDuration) {
+    /* El objeto playlist creado debe soportar (al menos):
+      * una propiedad name (string)
+      * un metodo duration() que retorne la duración de la playlist.
+      * un metodo hasTrack(aTrack) que retorna true si aTrack se encuentra en la playlist
+    */
+
+    // corregir ??????????????????????????????
+    /*
     let playlist = new Playlist(name);
     let ts = this.getTracksMatchingGenres(genresToInclude);
     
@@ -127,16 +163,9 @@ class UNQfy {
     playlist.tracks = ts;
 
     this.playlists.push(playlist);
+    */
   }
 
-  //----
-
-  del(){
-    this.artists = [];
-    this.albums = [];//TODO: Eliminar esta linea
-    this.tracks = [];//TODO: Eliminar esta linea
-    this.playlists = [];
-  }
 
   save(filename = 'unqfy.json') {
     new picklejs.FileSerializer().serialize(filename, this);
@@ -145,7 +174,7 @@ class UNQfy {
   static load(filename = 'unqfy.json') {
     const fs = new picklejs.FileSerializer();
     // TODO: Agregar a la lista todas las clases que necesitan ser instanciadas
-    const classes = [UNQfy, Artist, Album, Track, Playlist];
+    const classes = [UNQfy, Artist, Album, Track, Genre, PlayList];
     fs.registerClasses(...classes);
     return fs.load(filename);
   }
@@ -153,6 +182,6 @@ class UNQfy {
 
 // TODO: exportar todas las clases que necesiten ser utilizadas desde un modulo cliente
 module.exports = {
-  UNQfy,
+  UNQfy,Artist,Album,Track,Genre,PlayList,
 };
 
