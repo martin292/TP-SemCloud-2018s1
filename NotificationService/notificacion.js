ea2d728fc34bb9f37140c5109afc5b78e02ac85a
martin <martin.ramos292@gmail.com>
2018-06-30 16:30:42 -0300

    Actualizo

--- NotificationService/notificacion.js
+++ NotificationService/notificacion.js
@@ -1,4 +1,7 @@
 const unqfyAPI = require('./UNQfy/UNQfyAPI');
+const nodemailer = require('nodemailer');
+
+//---------------------------------------------------
 
 class Subscription{
     constructor(nameArtist, email){
@@ -12,3 +15,35 @@ class Notification{
         this.subscriptions = [];
     }
 }
+
+/*
+
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com', // server para enviar mail desde gmail
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: '<suCuentaDeGmail>',
+        pass: '<suPasswordEnTextoPlano>',
+    },
+});
+
+// setup email data with unicode symbols
+const mailOptions = {
+    from: '"Subscription Service" <subsctiptionService@example.com>', // sender address
+    to: 'mail1@gmail.com, mail2@hotmail.com', // list of receivers
+    subject: 'Asunto’, // Subject line
+    text: 'asdadadas', // plain text body
+    html: '<b>Hello world?</b>' // html body
+};
+
+// enviando mail con callbacks
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(info);
+});
+
+*/
