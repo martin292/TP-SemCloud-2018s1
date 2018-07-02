ae2ce60f31e90c1debbc9a6e2be71940c48cd89a
martin <martin.ramos292@gmail.com>
2018-07-02 15:41:14 -0300

    Agrego algunos servicios (sin testear)

--- NotificationService/notificacion.js
+++ NotificationService/notificacion.js
@@ -1,4 +1,4 @@
-const unqfyAPI = require('./UNQfy/UNQfyAPI');
+//const unqfyAPI = require('./UNQfy/UNQfyAPI');
 const nodemailer = require('nodemailer');
 
 //---------------------------------------------------
@@ -13,37 +13,62 @@ class Subscription{
 class Notification{
     constructor(){
         this.subscriptions = [];
+        this.rp = require('request-promise');
+    }
+
+    getArtistName(id){
+        let name;
+        const options = {
+            url: 'http://localhost:5000/api/artists/' + id,
+            method: 'GET',
+            json: true
+        };
+        rp.get(options).then((res) => name = res.name);
+        
+        return name;
+    }
+
+    addSubscription(name, email){
+        let sub = new Subscription(name, email);
+        if(!this.subscriptions.includes(sub)){
+            this.subscriptions.push(sub);
+        }
+    }
+
+    removeSubscription(name, email){
+        this.subscriptions = this.subscriptions.filter(sub => sub.name !== name && sub.email !== email);
+    }
+
+    notify(name, from, message, subject){
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com', // server para enviar mail desde gmail
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: '<suCuentaDeGmail>',
+                pass: '<suPasswordEnTextoPlano>',
+            },
+        });
+
+        const mailOptions = {
+            from: from, // sender address
+            to: this.emails(name), // list of receivers
+            subject: subject , // Subject line
+            text: message, // plain text body
+            html: '<b>Hello</b>' // html body
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(info);
+            }
+        });
+    }
+
+    emails(name){
+        return this.subscriptions.map(sub => sub.email).join();
     }
 }
 
-/*
-
-// create reusable transporter object using the default SMTP transport
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com', // server para enviar mail desde gmail
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: '<suCuentaDeGmail>',
-        pass: '<suPasswordEnTextoPlano>',
-    },
-});
-
-// setup email data with unicode symbols
-const mailOptions = {
-    from: '"Subscription Service" <subsctiptionService@example.com>', // sender address
-    to: 'mail1@gmail.com, mail2@hotmail.com', // list of receivers
-    subject: 'Asunto’, // Subject line
-    text: 'asdadadas', // plain text body
-    html: '<b>Hello world?</b>' // html body
-};
-
-// enviando mail con callbacks
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(info);
-});
-
-*/
--- NotificationService/notificacionAPI.js
+++ NotificationService/notificacionAPI.js
@@ -17,22 +17,29 @@ let port = process.env.PORT || 5001;
 
 
 //------------------------------------------------------------------
+let notificacion = new Notification();
 
 // POST /api/subscribe
 router.post('/suscribe', (req, res) => {
-      
+      let artist = notificacion.getArtistName(req.body.artistId);
+      notificacion.addSubscription(artist, req.body.email);
+      res.status(200);
 });
 
 
 // POST /api/unsubscribe
 router.post('/unsuscribe', (req, res) => {
-      
+      let artist = notificacion.getArtistName(req.body.artistId);
+      notificacion.removeSubsciption(artist, req.body.email);
+      res.status(200);
 });
 
 
 // POST /api/notify
 router.post('/notify', (req, res) => {
-      
+      let artist = notificacion.getArtistName(req.body.artistId);
+      notification.notify(artist, req.body.from, req.body.message, req.body.subject);
+      res.status(200);
 });
 
 
