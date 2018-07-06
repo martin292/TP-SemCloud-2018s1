662d3b39f5b58429aa28edaae18cfc562b1aab9c
marcelo <jmdicostanzo11@gmail.com>
2018-07-06 17:55:44 -0300

    errors

---
+++ NotificationService/errors.js
@@ -0,0 +1,55 @@
+class APIError extends Error { 
+    constructor(name, statusCode, errorCode, message = null) {
+        super(message || name);   
+        this.name = name;   
+        this.status = statusCode;   
+        this.errorCode = errorCode; } 
+}
+
+
+class BadRequest extends APIError { 
+    constructor() {  
+         super('BadRequest', 400, 'BAD_REQUEST'); 
+    } 
+}
+
+class ResourceNotFound extends APIError{
+    constructor() {  
+        super('ResourceNotFound', 404, 'RESOURCE_NOT_FOUND'); 
+   } 
+}
+
+class RelatedResourceNotFound extends APIError{
+    constructor() {  
+        super('RelatedResourceNotFound', 404, 'RELATED_RESOURCE_NOT_FOUND'); 
+   } 
+}
+
+class ResourceAlreadyExist extends APIError { 
+    constructor() {  
+         super('ResourceAlreadyExist', 409, 'RESOURCE_ALREADY_EXISTS'); 
+    } 
+}
+
+class InvalidURLError extends APIError { 
+    constructor() {  
+         super('InvalidURLError', 404, 'RESOURCE_NOT_FOUND'); 
+    } 
+}
+
+class InternalServerError extends APIError { 
+    constructor() {  
+         super('InternalServerError', 500, 'INTERNAL_SERVER_ERROR'); 
+    } 
+}
+
+
+module.exports = {
+    APIError: APIError,
+    BadRequest: BadRequest,
+    ResourceAlreadyExist: ResourceAlreadyExist,
+    InvalidURLError:InvalidURLError,
+    ResourceNotFound:ResourceNotFound,
+    RelatedResourceNotFound:RelatedResourceNotFound,
+    InternalServerError: InternalServerError
+  }
--- NotificationService/notificacionAPI.js
+++ NotificationService/notificacionAPI.js
@@ -10,6 +10,10 @@ app.use(bodyParser.json());
 let router = express.Router();
 let port = process.env.PORT || 5001;
 
+let errors = require('./errors');
+let ApiError = errors.APIError;
+let NotFound = errors.ResourceNotFound;
+
 //------------------------------------------------------------------
 
 const fs = require('fs');
@@ -27,6 +31,26 @@ function getNotificacion(filename) {
 function saveNotificacion(noti, filename) {
     console.log('Save!');
     noti.save(filename);
+}
+
+//------------------------------------------------------------------
+
+function errorHandler(err, req, res, next) {
+    console.error(err); // imprimimos el error en consola 
+    // Chequeamos que tipo de error es y actuamos en consecuencia
+    if(err instanceof ApiError  ){
+        res.status(err.status); 
+        res.json({status: err.status, errorCode: err.errorCode});
+    } else if (err.type === 'entity.parse.failed'){
+        // body-parser error para JSON invalido   
+        res.status(err.status);
+        res.json({status: err.status, errorCode: 'BAD_REQUEST'}); }
+      else {
+             // continua con el manejador de errores por defecto
+        res.status(err.status)
+        res.json({status: 500, errorCode: 'INTERNAL_SERVER_ERROR'})   
+        next(err); 
+    } 
 }
 
 //------------------------------------------------------------------
@@ -87,13 +111,15 @@ router.delete('/subscriptions', (req, res) => {
 });
 
 router.use('/*', (req, res) => {
-    res.status(404).json({ status: 404, errorCode: "RESOURCE_NOT_FOUND" });
+    throw new NotFound();
 });
 
 
 //------------------------------------------------------------------
 
 app.use('/api', router);
+
+app.use(errorHandler);
 
 app.listen(port);
 console.log('http://localhost:' + port + '/api');
